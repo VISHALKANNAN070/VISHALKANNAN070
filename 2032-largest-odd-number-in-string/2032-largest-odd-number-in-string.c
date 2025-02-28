68f8ba517b045f0070c127f89e581bd39fbae7fe
Vishal Kannan <vishalkannan070@gmail.com>
2025-02-28 22:44:45 +0530

    Time: 0 ms (100%), Space: 11 MB (39.25%) - LeetHub

--- 2032-largest-odd-number-in-string/2032-largest-odd-number-in-string.c
+++ 2032-largest-odd-number-in-string/2032-largest-odd-number-in-string.c
@@ -1,7 +1,7 @@
 char* largestOddNumber(char* num) {
     int len = strlen(num);
     for(int i=len-1;i>=0;i--){
-        if(num[i]%2 !=0){
+        if((num[i]-'0' )%2 !=0){
             return num;
         }
         else{
